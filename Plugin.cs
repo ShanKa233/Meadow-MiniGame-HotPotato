fb101ff05f127191a159b25d8ab7251c1d08ec34
ShanKa <Shan_Ka@outlook.com>
2025-04-18 16:13:55 +0200

    传递炸弹改为客机传递,强制取消了矛击伤害玩家

--- Plugin.cs
+++ Plugin.cs
@@ -56,6 +56,8 @@ namespace MiniGameHotPotato
                 //处理传递炸弹的碰撞事件
                 On.Player.Collide += Player_Collide;
 
+                On.Weapon.HitThisObject += Weapon_HitThisObject;
+
                 fullyInit = true;
             }
             catch (Exception e)
@@ -65,53 +67,64 @@ namespace MiniGameHotPotato
             }
         }
 
+        //防止在传炸弹模式用矛造成致命伤害
+        private bool Weapon_HitThisObject(On.Weapon.orig_HitThisObject orig, Weapon self, PhysicalObject obj)
+        {
+            if (RainMeadow.RainMeadow.isArenaMode(out var arena) && arena.onlineArenaGameMode is HotPotatoArena potatoArena)
+            {
+                bool num = obj is Player && self is Spear && self.thrownBy != null && self.thrownBy is Player;
+                if (num)return false;
+            }
+            return orig(self, obj);
+        }
+
+
         private void OnlineResource_OnAvailable(OnlineResource resource)
         {
             HotPotatoArena.bombData = resource.AddData(new BombGameData());
         }
-        
+
 
         private void Player_Collide(On.Player.orig_Collide orig, Player self, PhysicalObject otherObject, int myChunk, int otherChunk)
         {
             orig(self, otherObject, myChunk, otherChunk);
             if (RainMeadow.RainMeadow.isArenaMode(out var arena) && arena.onlineArenaGameMode is HotPotatoArena potatoArena)
             {
-                //只让主机处理防止反复传递
-                if (OnlineManager.lobby.isOwner && HotPotatoArena.bombData.passCD <= 0)
+
+                //如果炸弹是这个玩家而且CD小于0
+                if (HotPotatoArena.bombData.bombHolder.isMe//这个机子是炸弹的端口
+                && HotPotatoArena.bombData.bombHolderCache == self//这个碰撞的角色是炸弹的持有者
+                && HotPotatoArena.bombData.passCD <= 0)//传炸弹的CD小于0
                 {
-                    // 使用缓存系统检查自己是否是炸弹持有者
-                    if (HotPotatoArena.bombData.bombHolderCache == self)
+                    // 确保碰撞的是另一个玩家
+                    if (self.Consious && self.stun <= 2 && otherObject is Player otherPlayer && otherPlayer != self)
                     {
-                        // 确保碰撞的是另一个玩家
-                        if (self.Consious && self.stun <= 2 && otherObject is Player otherPlayer && otherPlayer != self)
+                        // 确保另一个玩家活着且能被传递炸弹
+                        if (otherPlayer.playerState.alive)
                         {
-                            // 确保另一个玩家活着且能被传递炸弹
-                            if (otherPlayer.playerState.alive)
+                            // 获取另一个玩家的OnlinePlayer实例
+                            if (OnlinePhysicalObject.map.TryGetValue(otherPlayer.abstractCreature, out var otherOnlineObject) &&
+                                otherOnlineObject != null && otherOnlineObject.owner != null)
                             {
-                                // 获取另一个玩家的OnlinePlayer实例
-                                if (OnlinePhysicalObject.map.TryGetValue(otherPlayer.abstractCreature, out var otherOnlineObject) &&
-                                    otherOnlineObject != null && otherOnlineObject.owner != null)
+                                // 更新炸弹计时器和持有者
+                                HotPotatoArena.bombData.nextBombTimer = Custom.IntClamp(HotPotatoArena.bombData.nextBombTimer / 40 - 5, 4, 30) * 40;
+                                HotPotatoArena.bombData.bombTimer = HotPotatoArena.bombData.nextBombTimer;
+                                HotPotatoArena.bombData.bombHolder = otherOnlineObject.owner;
+                                HotPotatoArena.bombData.bombHolderCache = otherPlayer; // 直接更新缓存
+
+                                // 传递炸弹的音效
+                                otherPlayer.room.PlaySound(SoundID.MENU_Add_Level, otherPlayer.firstChunk, false, 1, 2);
+                                // 传递炸弹的CD
+                                HotPotatoArena.bombData.passCD = 30;
+                                // 击晕新持有者防止反复触发
+                                otherPlayer.Stun(40);
+
+                                // 同步到其他玩家
+                                foreach (var player in OnlineManager.players)
                                 {
-                                    // 更新炸弹计时器和持有者
-                                    HotPotatoArena.bombData.nextBombTimer = Custom.IntClamp(HotPotatoArena.bombData.nextBombTimer / 40 - 5, 4, 30) * 40;
-                                    HotPotatoArena.bombData.bombTimer = HotPotatoArena.bombData.nextBombTimer;
-                                    HotPotatoArena.bombData.bombHolder = otherOnlineObject.owner;
-                                    HotPotatoArena.bombData.bombHolderCache = otherPlayer; // 直接更新缓存
-
-                                    // 传递炸弹的音效
-                                    otherPlayer.room.PlaySound(SoundID.MENU_Add_Level, otherPlayer.firstChunk, false, 1, 2);
-                                    // 传递炸弹的CD
-                                    HotPotatoArena.bombData.passCD = 10;
-                                    // 击晕新持有者防止反复触发
-                                    otherPlayer.Stun(40);
-
-                                    // 同步到其他玩家
-                                    foreach (var player in OnlineManager.players)
+                                    if (!player.isMe)
                                     {
-                                        if (!player.isMe)
-                                        {
-                                            player.InvokeOnceRPC(HotPotatoArenaRPCs.PassBomb, otherOnlineObject.owner);
-                                        }
+                                        player.InvokeOnceRPC(HotPotatoArenaRPCs.PassBomb, otherOnlineObject.owner);
                                     }
                                 }
                             }
--- obj/Debug/net48/Meadow MiniGame HotPotato.AssemblyInfo.cs
+++ obj/Debug/net48/Meadow MiniGame HotPotato.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniGameHotPotato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c0dff5666e65dfca400fcfc3426809d3cf01f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c5e6814c784c56011ef9b6c3fcadf118e0a0b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniGameHotPotato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniGameHotPotato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
