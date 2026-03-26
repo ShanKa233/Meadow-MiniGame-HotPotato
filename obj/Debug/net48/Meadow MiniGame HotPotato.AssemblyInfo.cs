7ba06cd24f638c38fcd18dc4b5a2fbb618ed0a89
ShanKa <Shan_Ka@outlook.com>
2026-03-26 09:11:53 +0100

    上传了创意工坊

--- obj/Debug/net48/Meadow MiniGame HotPotato.AssemblyInfo.cs
+++ obj/Debug/net48/Meadow MiniGame HotPotato.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniGameHotPotato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f73257283ade6b28c8a15b6be0927c48ca82b2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5ab473bb40e8c63fe47a59602bd3866f283997")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniGameHotPotato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniGameHotPotato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
